ffa2e689b4a0d22ba61ee1ae8dd19aabe827b586
Perfect Yayra Gidisu <36855498+perfectyayra@users.noreply.github.com>
2023-06-17 21:26:17 +0200

    Update deim.m

--- deim.m
+++ deim.m
@@ -3,6 +3,6 @@ function icol = deim(V,k)
 icol=zeros(1,k);
 [~, icol(1)] = max(abs(V(:,1)));
 for j = 2:k
-   V(:,j) = V(:,j) - V(:,1:j-1) * (V(icol(1:j-1),1:j-1) \ V(icol(1:j-1),j+1));
+   V(:,j) = V(:,j) - V(:,1:j-1) * (V(icol(1:j-1),1:j-1) \ V(icol(1:j-1),j));
    [~, icol(j)] = max(abs(V(:,j)));
 end
