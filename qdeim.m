fa16717254fdb9f6b199f332a8c76c24c0db15a8
Perfect Yayra Gidisu <36855498+perfectyayra@users.noreply.github.com>
2023-06-20 16:05:40 +0200

    Update qdeim.m

--- qdeim.m
+++ qdeim.m
@@ -1,6 +1,8 @@
 function irow = qdeim(U,k)
 
 %Index selection based on Q-DEIM
+% U is singular vectors
+% k desired number of indices
 %Reference: Gugercin and Drmac, 2015
 
 [~,~,p] = qr(U','vector') ; irow = p(1:k) ;
